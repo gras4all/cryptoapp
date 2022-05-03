123eab8a2e25f0be038cf6ff5af7e556c92d5781
Andrey Grunenkov <60237240+gras4all@users.noreply.github.com>
2022-05-03 09:29:59 +0400

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Андрей Груненков on 03.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
